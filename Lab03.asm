095142140264bff23f2740375a856c3f5867ea42
EmilioGiron <36040182+EmilioGiron@users.noreply.github.com>
2019-10-04 00:45:22 -0600

    Lab03 Terminado

---
+++ Lab03.asm
@@ -0,0 +1,133 @@
+.MODEL small
+.DATA
+
+	texto0 DB 10,13, 'Ejercicio 1: $'
+	texto1 DB 10,13, 'Ingresar numero 1: $'
+	texto2 DB 10,13, 'Ingresar numero 2: $'
+	texto3 DB 10,13, 'Total: $'
+	texto4 DB 10,13, 'Diferencia: $'
+	texto5 DB 10,13, 'Producto: $'
+	texto6 DB 10,13, 'Cociente: $'
+	texto7 DB 10,13, 'Residuo: $'
+	total DB ?
+	diferencia DB ?
+	producto DB ?
+	cociente DB ?
+	residuo DB ?
+	num1 DB ?		
+	num2 DB ?		;Inicializando la variable
+
+.STACK
+.CODE
+Programa:
+
+	MOV AX, @Data
+	MOV DS, AX
+
+	;Mostrar mensaje texto0
+	MOV DX, OFFSET texto0
+	MOV AH, 09h
+	INT 21h
+	
+	;Mostrar mensaje texto1
+	MOV DX, OFFSET texto1
+	MOV AH, 09h
+	INT 21h
+
+	;Leer numero 1:
+	XOR AX, AX
+	MOV AH, 01h
+	INT 21h
+	SUB AL, 30h
+	MOV num1, AL
+
+	;Mostrar mensaje texto2
+	XOR DX, DX
+	MOV DX, OFFSET texto2
+	MOV AH, 09h
+	INT 21h
+
+	;Leer numero 2:
+	XOR AX, AX
+	MOV AH, 01h
+	INT 21h
+	SUB AL, 30h
+	MOV num2, AL
+
+
+	;OPERACIONES ARITMETICAS
+
+	;Suma
+	XOR DX, DX
+	MOV DX, OFFSET texto3
+	MOV AH, 09h
+	INT 21h
+
+	XOR CX, CX
+	MOV CL, num1
+	ADD CL, num2
+	ADD CL, 30h
+	MOV DX, OFFSET CX
+	MOV AH, 02h
+	INT 21h
+
+	;Resta
+	XOR DX, DX
+	MOV DX, OFFSET texto4
+	MOV AH, 09h
+	INT 21h
+
+	XOR CX, CX
+	MOV CL, num1
+	SUB CL, num2
+	ADD CL, 30h
+	MOV DX, OFFSET CX
+	MOV AH, 02h
+	INT 21h
+
+	;Multiplicacion
+	XOR DX, DX
+	MOV DX, OFFSET texto5
+	MOV AH, 09h
+	INT 21h
+
+	XOR AX, AX
+	MOV AL, num1
+	MUL num2
+	ADD AL, 30h
+	MOV DX, OFFSET AX
+	MOV AH, 02h
+	INT 21h
+
+	;Division
+	XOR DX, DX
+	MOV DX, OFFSET texto6
+	MOV AH, 09h
+	INT 21h
+
+	XOR AX, AX
+	MOV AL, num1
+	DIV num2
+	XOR CX, CX
+	XOR BX, BX
+	MOV CL, AL
+	ADD CL, 30h
+	MOV BL, AH
+	ADD BL, 30h
+	MOV DX, OFFSET CX
+	MOV AH, 02h
+	INT 21h
+
+	XOR DX, DX
+	MOV DX, OFFSET texto7
+	MOV AH, 09h
+	INT 21h
+
+	MOV DX, OFFSET BX
+	MOV AH, 02h
+	INT 21h
+
+	;FINALIZAR
+	MOV AH, 4Ch
+	INT 21h	
+END Programa
